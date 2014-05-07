0733accf8dd2fe9bb725c5a04a48176d27d26690
m80126colin <m80126colin@gmail.com>
2014-05-08 02:59:23 +0800

    cache

---
+++ js/index.js
@@ -0,0 +1,71 @@
+$.getJSON('data/board.json', '', function(data) {
+	var decodeObj	= function(obj, args) {
+		var res		= {};
+		res.ver		= obj['version'];
+		res.date	= obj['date'];
+		res.cont	= obj['content'];
+		res.sum		= obj['summary'];
+		res.code	= obj['code'];
+		res.des		= obj['description'];
+		res.img 	= obj['img'] || 'img/blank.png';
+		res.args	= args;
+		return res;
+	}
+	var firstWidth		= {'w': '300px'};
+	var secondHeight	= {'h': '50px'};
+	var res =
+	'<div id="board">\
+		<h1>公告</h1><ul class="list-group">';
+	for (var i = 0; i < data.length; i++) {
+		var obj = decodeObj(data[i], firstWidth);
+		res += '<li class="list-group-item">\
+					<div class="media">';
+		// Start img
+		res += '<img class="media-object \
+					pull-right" src="' + obj.img + '"';
+		if (obj.args.h)
+			res += ' height="' + obj.args.h + '"';
+		if (obj.args.w)
+			res += ' width="' + obj.args.w + '"';
+		res += '>';
+		// End img
+		res += '<div class="media-body">';
+		if (obj.code) {
+			res +=
+				'<h3 class="page-header">版本 ' + obj.code + ' (' + obj.ver + ') <small>' + obj.date + '</small></h3>';
+		}
+		else {
+			res +=
+				'<h3 class="page-header">版本 ' + obj.ver + ' <small>' + obj.date + '</small></h3>';
+		}
+		if (obj.des) {
+			res += '<div><p class="paragraph">' + obj.des + '</p></div>';
+		}
+		if (obj.cont && obj.cont.length) {
+			res +=	'<ol><li>' + obj.cont.join('</li><li>') + '</li></ol>';
+		}
+		if (obj.sum && obj.sum.length) {
+			res +=	'<h4>子版本</h4>\
+					<table class="table table-hover">';
+			for (var j = 0; j < obj.sum.length; j++) {
+				var sObj = decodeObj(obj.sum[j], secondHeight);
+				res += '<tr>\
+							<th>' + sObj.ver + '</th>\
+							<td>' + sObj.date + '</td>';
+				if (sObj && sObj.cont.length) {
+					res +=	'<td><ul><li>' + sObj.cont.join('</li><li>') + '</li></ul></td>';
+				}
+				res += '<td>';
+				if (sObj.img != 'img/blank.png')
+					res += '<a href="' + sObj.img + '"><img src="' + sObj.img + '" height="' + sObj.args.h + '"></a>';
+				res += '</td></tr>';
+			}
+			res +=	'</table>';
+		}
+		res +=		'</div>\
+				</div>\
+			</li>';
+	}
+	res += '</ul></div>';
+	$('#result').append(res);
+});
--- js/solver.js
+++ js/solver.js
@@ -3,11 +3,13 @@
 //	
 //
 // ===================================================================
-var Util = function() {}
+var Util			= function() {}
 Util.transLang		= function(type) { return Util.lang[type]; }
 Util.transVerdit	= function(verd) { return Util.verdit[verd]; }
-Util.lang = ['', 'ANSI C', 'Java', 'C++', 'Pascal', 'C++11'];
-Util.verdit = {
+Util.lang			= [
+	'', 'ANSI C', 'Java', 'C++', 'Pascal', 'C++11'
+];
+Util.verdit			= {
 	0:	'沒有資料',
 	10:	'系統錯誤 (System Error)',
 	15:	'無法評定 (Cannot be Judged)',
@@ -22,7 +24,7 @@ Util.verdit = {
 	80:	'格式錯誤 (Presentation Error)',
 	90:	'正確 (Accepted)',
 };
-Util.btnStyle = {
+Util.btnStyle		= {
 	0: 'default',	10: 'info',		15: 'info',		20: 'info',
 	30: 'warning',	35: 'info',		40: 'primary',	45: 'primary',
 	50: 'primary',	60: 'primary',	70: 'danger',	80: 'warning',
@@ -34,52 +36,53 @@ Util.btnStyle = {
 //	Submit 物件
 //
 // ===================================================================
-var Submit = function(data) {
-	this.sub = data;
-	this.sub[4] = new Date(this.sub[4] * 1000);
+var Submit		= function(data) {
+	this.sub	= data;
+	this.sub[4]	= new Date(this.sub[4] * 1000);
 }
-Submit.prototype = {
-	getSid:			function() { return this.sub[0]; },
-	getPid:			function() { return this.sub[1]; },
-	getVerdit:		function() { return this.sub[2]; },
-	getRuntime:		function() { return this.sub[3]; },
-	getTime:		function() { return this.sub[4]; },
-	getLang:		function() { return this.sub[5]; },
-	getRank:		function() { return this.sub[6]; },
-	transVerdit:	function() { return Util.transVerdit(this.sub[2]); },
-	transLang:		function() { return Util.transLang(this.sub[5]); },
-	transRuntime:	function() {
-		var res = '0.';
-		for (var i = 100; i > 1; i /= 10) {
-			if (this.sub[3] < i)
-				res += '0';
-		}
-		res += this.sub[3];
-		return res;
-	},
-	transTime:		function() {
-		var date = this.sub[4];
-		var tmp = -date.getTimezoneOffset();
-		var aff = ['', '', 'GMT' + ((tmp < 0)? '-': '+')];
-		if (tmp < 0) tmp = -tmp;
-		var sep = ['-', ':', ''];
-		var time = [
-			[date.getFullYear(),	date.getMonth() + 1,	date.getDate()],
-			[date.getHours(),		date.getMinutes(),		date.getSeconds()],
-			[Math.floor(tmp / 60),	tmp % 60]
-		];
-		for (var i in time) {
-			for (var j in time[i]) {
-				time[i][j] = time[i][j].toString();
-				while (time[i][j].length < 2)
-					time[i][j] = '0' + time[i][j];
-			}
-			time[i] = aff[i] + time[i].join(sep[i]);
-		}
-		return time.join(' ');
+Submit.prototype.getSid			= function() { return this.sub[0]; }
+Submit.prototype.getPid			= function() { return this.sub[1]; }
+Submit.prototype.getVerdit		= function() { return this.sub[2]; }
+Submit.prototype.getRuntime		= function() { return this.sub[3]; }
+Submit.prototype.getTime		= function() { return this.sub[4]; }
+Submit.prototype.getLang		= function() { return this.sub[5]; }
+Submit.prototype.getRank		= function() { return this.sub[6]; }
+Submit.prototype.transVerdit	= function() {
+	return Util.transVerdit(this.sub[2]);
+}
+Submit.prototype.transLang		= function() {
+	return Util.transLang(this.sub[5]);
+}
+Submit.prototype.transRuntime	= function() {
+	var res = '0.';
+	for (var i = 100; i > 1; i /= 10) {
+		if (this.sub[3] < i)
+			res += '0';
 	}
-};
-
+	res += this.sub[3];
+	return res;
+}
+Submit.prototype.transTime		= function() {
+	var date = this.sub[4];
+	var tmp = -date.getTimezoneOffset();
+	var aff = ['', '', 'GMT' + ((tmp < 0)? '-': '+')];
+	if (tmp < 0) tmp = -tmp;
+	var sep = ['-', ':', ''];
+	var time = [
+		[date.getFullYear(),	date.getMonth() + 1,	date.getDate()],
+		[date.getHours(),		date.getMinutes(),		date.getSeconds()],
+		[Math.floor(tmp / 60),	tmp % 60]
+	];
+	for (var i in time) {
+		for (var j in time[i]) {
+			time[i][j] = time[i][j].toString();
+			while (time[i][j].length < 2)
+				time[i][j] = '0' + time[i][j];
+		}
+		time[i] = aff[i] + time[i].join(sep[i]);
+	}
+	return time.join(' ');
+}
 // ===================================================================
 //
 //	Problem 物件
@@ -106,60 +109,64 @@ var Problem = function(data) {
 						0,		// rank
 						[]);	// translate
 }
-Problem.prototype = {
-	getPid:			function() { return this.prob[0]; },
-	getNumber:		function() { return this.prob[1]; },
-	getTitle:		function() { return this.prob[2]; },
-	getCE:			function() { return this.prob[10]; },
-	getRF:			function() { return this.prob[11]; },
-	getRE:			function() { return this.prob[12]; },
-	getOLE:			function() { return this.prob[13]; },
-	getTLE:			function() { return this.prob[14]; },
-	getMLE:			function() { return this.prob[15]; },
-	getWA:			function() { return this.prob[16]; },
-	getPE:			function() { return this.prob[17]; },
-	getAC:			function() { return this.prob[18]; },
-	getCategory:	function() { return this.prob[21]; },
-	getSubmit:		function() { return this.prob[22]; },
-	getVerdit:		function() { return this.prob[23]; },
-	getTime:		function() { return this.prob[24]; },
-	getRank:		function() { return this.prob[25]; },
-	getTranslate:	function() { return this.prob[26]; },
-	setVerdit:		function(verd) { this.prob[23] = verd; },
-	setTime:		function(stmp) { this.prob[24] = stmp; },
-	setRank:		function(rank) { this.prob[25] = rank; },
-	getStyle:		function(args) {
-		var res = '';
-		var prob = this;
-		var val = prob.getNumber();
-		var query = (new Query()).addArgs({
-				'type': 'single',
-				'num': val
-			}).keepArgs(['user', 'type', 'num']);
-		if (args == undefined)
-			args = 'tc';
-		// 設定題號顏色
-		var btnAttr = 'btn-' + Util.btnStyle[ prob.getVerdit() ];
-		res += '<a id="uva' + val + '" href="problem.html?' + query + '" type="button" class="btn ' + btnAttr + '" target="_blank">' + val + ' ';
-		// 設定翻譯
-		if (args.indexOf('t') >= 0) {
-			var trans = prob.getTranslate();
-			for (var i = 0; i < trans.length; i++) {
-				res += '<span id="tooltip" class="glyphicon glyphicon-transfer" data-toggle="tooltip" data-original-title="' + trans[i] + '"></span>';
-			}
+Problem.prototype.getPid	= function() { return this.prob[0]; }
+Problem.prototype.getNumber	= function() { return this.prob[1]; }
+Problem.prototype.getTitle	= function() { return this.prob[2]; }
+Problem.prototype.getCE		= function() { return this.prob[10]; }
+Problem.prototype.getRF		= function() { return this.prob[11]; }
+Problem.prototype.getRE		= function() { return this.prob[12]; }
+Problem.prototype.getOLE	= function() { return this.prob[13]; }
+Problem.prototype.getTLE	= function() { return this.prob[14]; }
+Problem.prototype.getMLE	= function() { return this.prob[15]; }
+Problem.prototype.getWA		= function() { return this.prob[16]; }
+Problem.prototype.getPE		= function() { return this.prob[17]; }
+Problem.prototype.getAC		= function() { return this.prob[18]; }
+Problem.prototype.getCategory = function() { return this.prob[21]; }
+Problem.prototype.getSubmit	= function() { return this.prob[22]; }
+Problem.prototype.getVerdit	= function() { return this.prob[23]; }
+Problem.prototype.getTime	= function() { return this.prob[24]; }
+Problem.prototype.getRank	= function() { return this.prob[25]; }
+Problem.prototype.getTranslate = function() { return this.prob[26]; }
+Problem.prototype.setVerdit	= function(verd) {
+	this.prob[23] = verd;
+}
+Problem.prototype.setTime	= function(stmp) {
+	this.prob[24] = stmp;
+}
+Problem.prototype.setRank	= function(rank) {
+	this.prob[25] = rank;
+}
+Problem.prototype.getStyle	= function(args) {
+	var res = '';
+	var prob = this;
+	var val = prob.getNumber();
+	var query = (new Query()).addArgs({
+			'type': 'single',
+			'num': val
+		}).keepArgs(['user', 'type', 'num']);
+	if (args == undefined)
+		args = 'tc';
+	// 設定題號顏色
+	var btnAttr = 'btn-' + Util.btnStyle[ prob.getVerdit() ];
+	res += '<a id="uva' + val + '" href="problem.html?' + query + '" type="button" class="btn ' + btnAttr + '" target="_blank">' + val + ' ';
+	// 設定翻譯
+	if (args.indexOf('t') >= 0) {
+		var trans = prob.getTranslate();
+		for (var i = 0; i < trans.length; i++) {
+			res += '<span id="tooltip" class="glyphicon glyphicon-transfer" data-toggle="tooltip" data-original-title="' + trans[i] + '"></span>';
 		}
-		// 設定章節
-		if (args.indexOf('c') >= 0) {
-			var tags = prob.getCategory();
-			for (var i = 0; i < tags.length; i++) {
-				res += '<span id="tooltip" class="glyphicon glyphicon-tag" data-toggle="tooltip" data-original-title="' + tags[i] + '"></span>';
-			}
-		}
-		// 結尾
-		res += '</a>'
-		return res;
 	}
-};
+	// 設定章節
+	if (args.indexOf('c') >= 0) {
+		var tags = prob.getCategory();
+		for (var i = 0; i < tags.length; i++) {
+			res += '<span id="tooltip" class="glyphicon glyphicon-tag" data-toggle="tooltip" data-original-title="' + tags[i] + '"></span>';
+		}
+	}
+	// 結尾
+	res += '</a>'
+	return res;
+}
 
 // ===================================================================
 //
@@ -214,14 +221,14 @@ var Solver = function(args) {
 	/* ***************************************************** */
 	if (args == undefined)
 		args = {};
-	if (args['type'] == undefined)
-		args['type'] = 'all';
-	if (args['prob'] == undefined)
-		args['prob'] = true;
-	if (args['database'] == undefined)
-		args['database'] = true;
-	if (args['trans'] == undefined)
-		args['trans'] = true;
+	if (args.type == undefined)
+		args.type = 'all';
+	if (args.prob == undefined)
+		args.prob = true;
+	if (args.database == undefined)
+		args.database = true;
+	if (args.trans == undefined)
+		args.trans = true;
 
 	/* ***************************************************** */
 	/*                                                       */
