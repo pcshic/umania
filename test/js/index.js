f17557a93f2af8f2cc231796ea1180b388528ec6
m80126colin <m80126colin@gmail.com>
2014-06-07 19:06:08 +0800

    Change format about board

--- test/js/index.js
+++ test/js/index.js
@@ -1,71 +1,118 @@
-$.getJSON('data/board.json', '', function(data) {
-	var decodeObj	= function(obj, args) {
-		var res		= {};
-		res.ver		= obj['version'];
-		res.date	= obj['date'];
-		res.cont	= obj['content'];
-		res.sum		= obj['summary'];
-		res.code	= obj['code'];
-		res.des		= obj['description'];
-		res.img 	= obj['img'] || 'img/blank.png';
-		res.args	= args;
-		return res;
+var getImage = function(image, option) {
+	image = image || 'img/blank.png';
+	res = '<img src="' + image + '"';
+	for (var attr in option)
+		res += ' ' + attr + '="' + option[attr] + '"';
+	res += '>';
+	return res;
+}
+
+var getTitle = function(code, version, date) {
+	res = '<h3 class="page-header">版本 ';
+	if (code)
+		res += code + ' (' + version + ')';
+	else
+		res += version;
+	res += ' <small>' + date + '</small></h3>';
+	return res;
+}
+
+var getDescription = function(des) {
+	res = '';
+	if (des)
+		res += '<div><p class="paragraph">' + des + '</p></div>';
+	return res;
+}
+
+var getContent = function(content) {
+	res = '';
+	if (content) {
+		res += '<ol><li>';
+		if (content.length)
+			res += content.join('</li><li>');
+		else res += content;
+		res += '</li></ol>';
 	}
-	var firstWidth		= {'w': '300px'};
-	var secondHeight	= {'h': '50px'};
-	var res =
-	'<div id="board">\
-		<h1>公告</h1><ul class="list-group">';
-	for (var i = 0; i < data.length; i++) {
-		var obj = decodeObj(data[i], firstWidth);
-		res += '<li class="list-group-item">\
-					<div class="media">';
-		// Start img
-		res += '<img class="media-object \
-					pull-right" src="' + obj.img + '"';
-		if (obj.args.h)
-			res += ' height="' + obj.args.h + '"';
-		if (obj.args.w)
-			res += ' width="' + obj.args.w + '"';
-		res += '>';
-		// End img
-		res += '<div class="media-body">';
-		if (obj.code) {
-			res +=
-				'<h3 class="page-header">版本 ' + obj.code + ' (' + obj.ver + ') <small>' + obj.date + '</small></h3>';
-		}
-		else {
-			res +=
-				'<h3 class="page-header">版本 ' + obj.ver + ' <small>' + obj.date + '</small></h3>';
-		}
-		if (obj.des) {
-			res += '<div><p class="paragraph">' + obj.des + '</p></div>';
-		}
-		if (obj.cont && obj.cont.length) {
-			res +=	'<ol><li>' + obj.cont.join('</li><li>') + '</li></ol>';
-		}
-		if (obj.sum && obj.sum.length) {
-			res +=	'<h4>子版本</h4>\
-					<table class="table table-hover">';
-			for (var j = 0; j < obj.sum.length; j++) {
-				var sObj = decodeObj(obj.sum[j], secondHeight);
-				res += '<tr>\
-							<th>' + sObj.ver + '</th>\
-							<td>' + sObj.date + '</td>';
-				if (sObj && sObj.cont.length) {
-					res +=	'<td><ul><li>' + sObj.cont.join('</li><li>') + '</li></ul></td>';
-				}
-				res += '<td>';
-				if (sObj.img != 'img/blank.png')
-					res += '<a href="' + sObj.img + '"><img src="' + sObj.img + '" height="' + sObj.args.h + '"></a>';
-				res += '</td></tr>';
+	return res;
+}
+
+var getSubversionInfo = function(subinfo) {
+	res = '';
+	res += '\
+		<tr>\
+			<th>' + subinfo.version + '</th>\
+			<td>' + subinfo.date + '</td>\
+			<td>' + getContent(subinfo.content) + '</td>';
+	res += '<td>\
+				<a href="' + subinfo.image + '">' +
+				getImage(subinfo.image, { 'height': '50px' }) +
+				'</a>\
+			</td>\
+		</tr>';
+	return res;
+}
+
+var getSubversion = function(subver) {
+	var res = '';
+	if (subver) {
+		res += '\
+			<h4>子版本</h4>\
+			<table class="table table-hover">';
+			if (subver.length) {
+				for (var i = 0; i < subver.length; i++)
+					res += getSubversionInfo(subver[i]);
 			}
-			res +=	'</table>';
-		}
-		res +=		'</div>\
-				</div>\
-			</li>';
+			else res += getSubversionInfo(subver);
+		res += '\
+			</table>';
 	}
-	res += '</ul></div>';
-	$('#result').append(res);
+	return res;
+}
+
+var getVersionInfo = function(info) {
+	var res = '';
+	res += '\
+		<li class="list-group-item">\
+			<div class="media">';
+			res += getImage(info.image, {
+				'class': 'media-object pull-right',
+				'width': '300px'
+			});
+			res += '\
+				<div class="media-body">';
+				res += getTitle(info.code, info.version, info.date);
+				res += getDescription(info.description);
+				res += getContent(info.content);
+				res += getSubversion(info.subversion);
+	res += '\
+				</div>\
+			</div>\
+		</li>';
+	return res;
+}
+
+var getBoard = function(board) {
+	var res = '';
+	if (board) {
+		res += '\
+			<div id="board">\
+				<h1>公告</h1>\
+				<ul class="list-group">';
+				if (board.length) {
+					for (var i in board)
+						res += getVersionInfo(board[i]);
+				}
+				else res += getVersionInfo(board);
+		res += '\
+				</ul>\
+			</div>';
+	}
+	return res;
+}
+
+$(function() {
+	$.get('data/board.yml', '', function (data) {
+		var board = YAML.parse(data);
+		$('#result').append(getBoard(board));
+	}
 });
