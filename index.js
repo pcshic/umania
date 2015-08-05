d695b3b997bc111b78984f4ab260db095ee4de31
m80126colin <m80126colin@gmail.com>
2015-08-05 09:18:48 +0800

    QAQ

--- index.js
+++ index.js
@@ -1,23 +1,92 @@
 (function() {
+
   var DEBUG = 1;
+
+/* ******************************************************************** */
+/*                                                                      */
+/*  class: Utility                                                      */
+/*  member functions:                                                   */
+/*    none(o)                                                           */
+/*    isFunction(f)                                                     */
+/*                                                                      */
+/* ******************************************************************** */
+
+  function Utility() {}
+
   // ================================================================
   //
-  // function: none
+  //  function: none
   //
   // ================================================================
-  var none = function(o) {
+  Utility.prototype.none = function(o) {
     return typeof(o) === 'undefined';
   }
   // ================================================================
+  //
+  //  function: isFunction
+  //
   // ================================================================
-  var isFunction = function(f) {
+  Utility.prototype.isFunction = function(f) {
     var g = {};
     return f && g.toString.call(f) === '[object Function]';
   }
-  // ================================================================
-  // ================================================================
-  function savingBox() {
+  var UNIT = new Utility();
+
+/* ******************************************************************** */
+/* ******************************************************************** */
+
+  function UserManager() {}
+
+  UserManager.prototype.current = 'umania-current-user';
+  UserManager.prototype.setting = 'umania-user-setting';
+  UserManager.prototype.checkSetting = function(name) {
+    var mgr = this, res;
+    if ( UNIT.none(localStorage[mgr.setting]) ) {
+      res = {};
+    }
+    else {
+      res = JSON.parse(localStorage[mgr.setting]);
+    }
+    if ( UNIT.none(res[name]) )
+      res[name] = {};
+    localStorage[mgr.setting] = JSON.stringify(res);
   }
+  UserManager.prototype.getCurrentUser = function() {
+    return localStorage[this.current];
+  }
+  UserManager.prototype.setCurrentUser = function(name) {
+    var mgr = this;
+    if (!UNIT.none(name) && name !== '') {
+      localStorage[mgr.current] = name;
+      mgr.checkSetting(name);
+    }
+  }
+  UserManager.prototype.getUserId = function(name, cb) {
+    var mgr     = this;
+    mgr.checkSetting(name);
+    var setting = JSON.parse(localStorage[mgr.setting]);
+    if ( UNIT.none(setting[name]['id']) ) {
+      $.getJSON('http://uhunt.felix-halim.net/api/uname2uid/' + name, function (id) {
+          if (DEBUG) {
+            console.log('user id: ' + id);
+          }
+          if ( UNIT.isFunction(cb) ) cb(id);
+          setting[name]['id'] = id;
+          localStorage[mgr.setting] = JSON.stringify(setting);
+      });
+    }
+    else {
+      if (DEBUG) {
+        console.log('user id: ' + setting[name]['id']);
+      }
+      if ( UNIT.isFunction(cb) ) cb(setting[name]['id']);
+    }
+  }
+  var BOX = new UserManager();
+
+/* ******************************************************************** */
+/* ******************************************************************** */
+
   // ================================================================
   //
   //  function: randomColor
@@ -34,8 +103,8 @@
       'red', 'orange', 'yellow', 'olive', 'green', 'teal',
       'blue', 'purple', 'violet', 'pink', 'brown', 'grey'];
     var res = [];
-    if ( !none(options) ) {
-      if ( !none(options.except) ) {
+    if ( !UNIT.none(options) ) {
+      if ( !UNIT.none(options.except) ) {
         for (var i in colors) {
           var flag = 1;
           for (var j in options.except) {
@@ -51,6 +120,29 @@
   }
   // ================================================================
   // ================================================================
+  var getJudgeColor = function(code) {
+    var colors = {
+      10 : 'purple', // Submission error
+      15 : 'black',  // Can't be judged
+      20 : 'black',  // In queue
+      30 : 'yellow', // Compile error
+      35 : 'orange', // Restricted function
+      40 : 'teal',   // Runtime error
+      45 : 'blue',   // Output limit
+      50 : 'blue',   // Time limit
+      60 : 'blue',   // Memory limit
+      70 : 'red',    // Wrong answer
+      80 : 'pink',   // PresentationE
+      90 : 'green'   // Accepted
+    }
+    return colors[code];
+  }
+
+/* ******************************************************************** */
+/* ******************************************************************** */
+
+  // ================================================================
+  // ================================================================
   var addTab = function(prob) {
     if (DEBUG)
       console.log('start add tab.');
@@ -58,7 +150,7 @@
         num = $(prob).data('num');
     /* add tab */
     var tab = [];
-    tab.push('<a class="item" data-tab="uva' + id + '">');
+    tab.push('<a id="tab-uva' + id + '" class="item" data-tab="uva' + id + '">');
     tab.push('<i class="tag icon"></i>');
     tab.push('UVa ' + num);
     tab.push('</a>');
@@ -68,13 +160,52 @@
     else $('#menu').append(tab.join(''));
     /* add content */
     var content = [];
-    content.push('<section id="problem" class="ui bottom attached loading tab segment" data-tab="uva' + id + '"></section>');
+    content.push('<section id="problem-uva' + id + '" class="ui bottom attached loading tab segment" data-tab="uva' + id + '"></section>');
     $('#content').append(content.join(''));
     /* loading problem */
-    $.getJSON('')
+    $.getJSON('http://uhunt.felix-halim.net/api/p/id/' + id,
+      function (data) {
+        var art = [];
+        art.push('<article>');
+        art.push('XDDDDD');
+        art.push('</article>');
+        $('#problem-uva' + id).removeClass('loading').append(art.join(''));
+    });
     /* toogle tab */
     $('.tabular.menu .item').tab();
   }
+  // ================================================================
+  // ================================================================
+  var getSubmission = function() {
+    var user = BOX.getCurrentUser();
+    if (DEBUG) {
+      console.log('start loading submissions.');
+      console.log('current user: ' + user);
+    }
+    BOX.getUserId(user, function (id) {
+      var url = 'http://uhunt.felix-halim.net/api/subs-user/' + id;
+      $.getJSON(url, function (data) {
+        /* process submissions */
+        $.each(data.subs, function (i, sub) {
+          var state = $('#uva' + sub[1]).data('state');
+          if (UNIT.none(state) || state < sub[2])
+            state = sub[2];
+          $('#uva' + sub[1]).data('state', state);
+        });
+        /* add state */
+        $.each($('div.problem'), function (i, btn) {
+          var state = $(btn).data('state');
+          if ( !UNIT.none(state) ) {
+            $(btn).removeClass('basic').addClass( getJudgeColor(state) );
+          }
+        });
+      });
+    });
+  }
+
+/* ******************************************************************** */
+/* ******************************************************************** */
+
   // ================================================================
   //
   //  function: preProblemInitialize
@@ -96,7 +227,7 @@
     $('#userform').submit(function (e) {
       e.preventDefault();
       var name = $('#username').val();
-      if (name !== '') localStorage.uvaname = name;
+      BOX.setCurrentUser(name);
     });
     /* --------------------------------------------------------- */
     /*  remove 'loading' class after data is loaded              */
@@ -129,9 +260,13 @@
     /* --------------------------------------------------------- */
     /* --------------------------------------------------------- */
     $('.problem').click(function (e) {
+      var btn = this;
       if (DEBUG)
-        console.log($(this));
-      addTab(this);
+        console.log($(btn));
+      var id  = $(btn).data('id'),
+          tab = $('#tab-uva' + id);
+      if ( !tab.length )
+        addTab(btn);
     });
     /* --------------------------------------------------------- */
     /* --------------------------------------------------------- */
@@ -146,7 +281,7 @@
   // ================================================================
   $.getJSON('http://uhunt.felix-halim.net/api/p',
     function (data) {
-      if ( none(data) )
+      if ( UNIT.none(data) )
         throw 'error retrieve problem data';
       preProblemInitialize();
       var res = {};
@@ -155,7 +290,7 @@
       /* --------------------------------------------------------- */
       $.each(data, function (i, prob) {
         var num = Math.floor(prob[1] / 100);
-        if ( none(res[num]) )
+        if ( UNIT.none(res[num]) )
           res[num] = [];
         res[num].push(prob);
       });
@@ -197,49 +332,21 @@
         $('#category').append(nav.join(''));
         */
       });
+      $.get('./data/translate.yml', function (str) {
+        data = YAML.parse(str);
+        if (DEBUG)
+          console.log(data);
+      });
       /* --------------------------------------------------------- */
       /*  get username
       /* --------------------------------------------------------- */
-      var user = localStorage.uvaname;
-      if ( none(user) ) $('#info').append('無');
-      else              $('#info').append(user);
-      /* --------------------------------------------------------- */
-      /*  get submission
-      /* --------------------------------------------------------- */
-      $.getJSON('http://uhunt.felix-halim.net/api/uname2uid/' + user,
-        function (id) {
-          $.getJSON('http://uhunt.felix-halim.net/api/subs-user/' + id,
-            function (data) {
-              $.each(data.subs, function (i, sub) {
-                var state = $('#uva' + sub[1]).data('state');
-                if (none(state) || state < sub[2])
-                  state = sub[2];
-                $('#uva' + sub[1]).data('state', state);
-              });
-
-              var color = {
-                10 : 'purple', //Submission error
-                15 : 'black',  // Can't be judged
-                20 : 'black',  // In queue
-                30 : 'yellow', // Compile error
-                35 : 'orange', // Restricted function
-                40 : 'teal',   // Runtime error
-                45 : 'blue',   // Output limit
-                50 : 'blue',   // Time limit
-                60 : 'blue',   // Memory limit
-                70 : 'red',    // Wrong answer
-                80 : 'pink',   // PresentationE
-                90 : 'green'   // Accepted
-              }
-              /* add state */
-              $.each($('div.problem'), function (i, btn) {
-                var state = $(btn).data('state');
-                if ( !none(state) ) {
-                  $(btn).removeClass('basic').addClass(color[state]);
-                }
-              });
-            });
-        });
+      var user = BOX.getCurrentUser();
+      if ( UNIT.none(user) || !user ) $('#info').append('無');
+      else {
+        $('#info').append(user);
+        /* get submission */
+        getSubmission();
+      }
       postProblemInitialize();
     });
 }())
